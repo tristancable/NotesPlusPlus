b426ea67d6f798b2a24feaf1cfcc00fd3cb7185f
EmberXxX <amberoliver2005@gmail.com>
2025-02-13 17:54:44 -0700

    Themes are done

---
+++ Notes++/wwwroot/js/theme.js
@@ -0,0 +1,18 @@
+﻿window.setTheme = (cssFile) => {
+    let existingLink = document.getElementById("theme-css");
+    if (!existingLink) {
+        existingLink = document.createElement("link");
+        existingLink.id = "theme-css";
+        existingLink.rel = "stylesheet";
+        document.head.appendChild(existingLink);
+    }
+    existingLink.href = cssFile;
+};
+
+window.saveThemeToSession = (theme) => {
+    sessionStorage.setItem("selectedTheme", theme);
+};
+
+window.getThemeFromSession = () => {
+    return sessionStorage.getItem("selectedTheme") || "css/default.css";
+};
