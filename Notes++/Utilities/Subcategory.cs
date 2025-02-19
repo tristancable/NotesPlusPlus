c984f6726c278ae5ca173dbe90b128510c4202d6
Xenny76 <dredford@student.neumont.edu>
2025-02-19 15:47:04 -0700

    Added subcategory class

---
+++ Notes++/Utilities/Subcategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Notes__.Utilities
+{
+    class Subcategory
+    {
+        public string Name { get; set; }
+        public List<Note> notes { get; set; }
+        public Subcategory(string name)
+        {
+            Name = name;
+            notes = new List<Note>();
+        }
+    }
+}
