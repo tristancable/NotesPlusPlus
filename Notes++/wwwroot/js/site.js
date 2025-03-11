7b30363e92419f27204c4bec13dbf79fbc060031
tristancable <tcable@student.neumont.edu>
2025-03-11 09:03:51 -0600

    Changes?

--- Notes++/wwwroot/js/site.js
+++ Notes++/wwwroot/js/site.js
@@ -1,3 +1,13 @@
 ﻿window.preventDefaultDrop = function (event) {
     event.preventDefault();
 };
+
+async function chooseDirectory() {
+    try {
+        const handle = await window.showDirectoryPicker();
+        return handle ? handle.name : "";
+    } catch (error) {
+        console.error("Directory selection was canceled or failed.", error);
+        return "";
+    }
+}
